f6ce61f28c2ca0bb49c8784b1b769a753c3d0171
Chaluchukwu <chaliman.co@gmail.com>
2023-05-01 09:20:22 +0100

    added router guards

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -1,28 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Login from '../pages/Login.vue'
-import Signup from '../pages/Signup.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../pages/Home.vue"
+import Login from "../pages/Login.vue"
+import Signup from "../pages/Signup.vue"
+import store from '../store/store'
+import { AuthenticationError, getProfile, setPageTitle, errorNotification, ServerError, NetworkError } from '../util'
+
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     component: Login,
     meta: {
-      title: 'Login'
+      title: "Login"
     }
   },
   {
-    path: '/signup',
-    name: 'Signup',
+    path: "/signup",
+    name: "Signup",
     component: Signup,
     meta: {
-      title: 'Create Account'
+      title: "Create Account"
     }
   },
 
@@ -32,4 +35,25 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+router.beforeEach(async (to, from, next) => {
+  if (!profileIsSet()) {
+    try {
+      store.commit("set_profile", await getProfile())
+    } catch (err) {
+      console.log(err)
+      if (err instanceof AuthenticationError && requiresAuthentication(to)) return next("/login")
+      if (err instanceof NetworkError) errorNotification("Error Communicating With The Server. Please try again")
+      else if (err instanceof ServerError) errorNotification("Internal Server Error")
+      if (requiresAuthentication(to)) return next(err)
+    }
+  }
+  setPageTitle(to.meta && to.meta.title)
+  next()
+})
+function requiresAuthentication (route) {
+  return route.meta && route.meta.privileges && (~route.meta.privileges.indexOf("authenticated") || ~route.meta.privileges.indexOf("admin"))
+}
+function profileIsSet () {
+  return store.state.profile
+}
 export default router
